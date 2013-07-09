fd9fff54a76459d3a01667ed48151462cf341a2c
joeyfreelandjr@gmail.com <joeyfreelandjr@gmail.com@ef7bcb1b-4383-5e20-0bb8-6684ecb6101c>
2013-07-09 21:10:25 +0000

    Added constant start bytes

--- HF80/Message.cs
+++ HF80/Message.cs
@@ -9,6 +9,14 @@ namespace HF80
     /* All methods and fields are static, so no instance of Message is needed */
     class Message
     {
+        public const HF80Address = 3; // The address of the HF80
+        
+        /* Defines the first character in each word */
+        public const byte WORD_ONE_START   = 0xC0 | HF80Address;
+        public const byte WORD_TWO_START   = 0xD0 | HF80Address;
+        public const byte WORD_THREE_START = 0xE0 | HF80Address;
+        public const byte WORD_FOUR_START  = 0xF0 | HF80Address;
+        
         public static byte[] SET_MODE_AM = new byte[] {};
         public static byte[] SET_MODE_LSB = new byte[] {};
         public static byte[] SET_MODE_ISB = new byte[] {};
